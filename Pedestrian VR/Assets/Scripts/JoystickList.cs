89911e95b1e7e40a0428a1f5b0a9033d3e148a56
Marc Fabián i Vilà <marcfv97@gmail.com>
2020-03-17 18:10:13 +0100

    First Commit

---
+++ Pedestrian VR/Assets/Scripts/JoystickList.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+public class JoystickList : MonoBehaviour
+{
+    // Prints input if is detected.
+
+    void Update()
+    {
+        if(Input.GetAxis("Joystick") != 0) //Axis Joystic just captures joystic rotation LEFT & RIGHT (-1/1)
+        {
+            Debug.Log("YEA" + Input.GetAxis("Joystick").ToString());
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            Debug.Log("Vertical = " + Input.GetAxis("Vertical").ToString());
+        }
+        if (Input.GetAxis("Horitzontal") != 0)
+        {
+            Debug.Log("Horitzontal = " + Input.GetAxis("Horitzontal").ToString());
+        }
+    }
+}
